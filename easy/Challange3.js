85a3fe9bc2ff9b81033fa84ed9806c6501346fae
Mandar deshmukh <128784537+Mandar1117@users.noreply.github.com>
2023-09-05 17:18:29 +0530

    Challange 3 updated

---
+++ easy/Challange3.js
@@ -0,0 +1,13 @@
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+function addition(num1,num2)
+{
+    let result=num1+num2;
+    if (result<100)
+    {
+        return true
+    }
+    else{
+        return false  
+    }
+}
