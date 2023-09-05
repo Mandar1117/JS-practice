b54b4184a934487bae16bd8d482252c2e9eed1a4
Mandar deshmukh <128784537+Mandar1117@users.noreply.github.com>
2023-09-05 18:35:57 +0530

    Challange 5 updated

---
+++ easy/Challange5.js
@@ -0,0 +1,18 @@
+//Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+
+function largestSwap(num)
+{
+    let initial=num;
+    let temp=num;
+    num=num%10;
+    temp=Math.floor(temp/10);
+    num=(num*10)+temp;
+    if(initial<num)
+    {
+        document.write(false);
+    }
+    else
+    {
+        document.write(true);
+    }
+}
