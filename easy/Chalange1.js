f9d0e2ceaf6c296f58ebab519f02a36cfe0f49b4
Mandar deshmukh <128784537+Mandar1117@users.noreply.github.com>
2023-09-05 22:31:30 +0530

    commit id 01

--- easy/Chalange1.js
+++ easy/Chalange1.js
@@ -3,10 +3,16 @@
 function add(num)
 {
 let temp = num;
-while(num>0)
-{
- temp=temp+(num-1);
- num--
+// while(num>0)
+// {
+//  temp=temp+(num-1);
+//  num--
+// }
+// document.write(temp)
+
+for (let num = temp; num > 0; num--) {
+    temp=temp+(num-1)
 }
-document.write(temp)
+document.write(temp);
 }
+
