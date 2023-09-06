c80b5b574488ea8969fae231fe85bc7f896982a6
Mandar deshmukh <128784537+Mandar1117@users.noreply.github.com>
2023-09-06 10:42:24 +0530

    updated

--- easy/Challange3.js
+++ easy/Challange3.js
@@ -2,12 +2,8 @@
 
 function addition(num1,num2)
 {
-    let result=num1+num2;
-    if (result<100)
-    {
-        return true
-    }
-    else{
-        return false  
-    }
+    const result=num1+num2;
+    if (result<100) return true
+    
+    return false  
 }
