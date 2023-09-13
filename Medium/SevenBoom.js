afe1b612d0d1d14c7e8e6e006203db2b11253cea
Mandar deshmukh <128784537+Mandar1117@users.noreply.github.com>
2023-09-13 20:03:57 +0530

    SevenBoom updated

--- Medium/SevenBoom.js
+++ Medium/SevenBoom.js
@@ -3,15 +3,7 @@
 
 const sevenBoom=(arr)=>
 {
-    let temp;
-    for (let i = 0; i < arr.length; i++) {
-        
-        if (arr[i]==7) 
-        {
-            temp=arr[i]
-        }
-    }
-    if (temp==7) return("Boom!")
+    if (arr.includes(7)) return("Boom!")
 
     else  return("there is no 7 in the array")
 
